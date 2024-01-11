69e40ea80f2278cba918d343de6daa322d684a9a
Valdiq <stasyshyn.vld@gmail.com>
2024-01-11 16:17:09 +0100

    init

---
+++ web-server/src/main/java/org/vlad_stasyshyn/AddressBookWebServerApp.java
@@ -0,0 +1,12 @@
+package org.vlad_stasyshyn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class AddressBookWebServerApp {
+    public static void main(String[] args) {
+        SpringApplication.run(AddressBookWebServerApp.class, args);
+    }
+}
